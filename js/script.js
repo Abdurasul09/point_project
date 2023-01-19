d90793c8d207ab0a495643458068c3f014b76f1d
Abdurasul <ahmedovabdurasul19@gmail.com>
2023-01-19 21:13:51 +0100

    finish

--- js/script.js
+++ js/script.js
@@ -1,32 +1,49 @@
 // GLOBAL VARIABLES
 const canvas = document.querySelector("canvas"),
   toolBtns = document.querySelectorAll(".tool"),
-  fillColor = document.querySelector("#fill-color");
+  fillColor = document.querySelector("#fill-color"),
+	sizeSlider = document.querySelector("#size-slider"),
+	colorsBtns = document.querySelectorAll(".colors .option"),
+	colorPicker = document.querySelector("#color-picker"),
+	clearCanvasBtn = document.querySelector(".clear-canvas"),
+	saveImgBtn = document.querySelector(".save-img");
 
-// VARIABLES
+// VARIABLES WITH DEFAULT VALUES
 let ctx = canvas.getContext("2d"),
   isDrawing = false,
   brushWidth = 5,
   selectedTool = "brush",
+	selectedColor = "#000",
   prevMouseX,
   prevMouseY,
   snapshot;
+
+	// SET CANVAS BACKGROUND
+
+	const setCanvasBg = () =>{
+		ctx.fillStyle = "#fff";
+		ctx.fillRect(0, 0, canvas.width, canvas.height);
+		ctx.fillStyle = selectedColor
+	}
 
 // SET CANVAS WIDTH AND HIGHT
 window.addEventListener("load", () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
   console.log(canvas.height);
+	setCanvasBg()
 });
 
 // START DRAW
 const startDraw = (e) => {
   isDrawing = true;
-  ctx.beginPath();
   ctx.lineWidth = brushWidth;
   prevMouseX = e.offsetX;
   prevMouseY = e.offsetY;
   snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  ctx.beginPath();
+	ctx.strokeStyle = selectedColor;
+	ctx.fillStyle = selectedColor;
 };
 
 // DRAW RECTANGLE
@@ -56,6 +73,18 @@ const drawCircle = (e) => {
 	fillColor.checked ? ctx.fill() : ctx.stroke();
 };
 
+// DRAW TRIANGLE
+
+const drawTriangle = (e) => {
+  ctx.beginPath();
+  ctx.moveTo(prevMouseX, prevMouseY);
+	ctx.lineTo(e.offsetX, e.offsetY);
+	ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+	ctx.closePath();
+	ctx.stroke();
+	fillColor.checked ? ctx.fill() : ctx.stroke();
+}
+
 // DRAWING
 const drawing = (e) => {
   if (!isDrawing) return;
@@ -71,8 +100,54 @@ const drawing = (e) => {
     case "circle":
       drawCircle(e);
       break;
+		case "triangle":
+			drawTriangle(e);
+			break
+		case "eraser": 
+		  ctx.strokeStyle = "#fff"
+			ctx.lineTo(e.offsetX, e.offsetY);
+      ctx.stroke();
+		  break
+		default: 
+		   break
   }
 };
+
+// CHANGE BRUSH WIDTH
+
+sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value)
+
+// SET COLOR TO SHIPS
+colorsBtns.forEach(btn => {
+	btn.addEventListener("click", e => {
+		document.querySelector(".options .selected").classList.remove("selected");
+    btn.classList.add("selected");
+		const bgColor = window.getComputedStyle(btn).getPropertyValue("background-color")
+		selectedColor = bgColor;
+		console.log(bgColor);
+	})
+})
+
+// SET COLOR FROM COLOR PICKER
+colorPicker.addEventListener("change", () => {
+  colorPicker.parentElement.style.background = colorPicker.value
+	colorPicker.parentElement.click()
+})
+
+// CLEAR CANVAS BTN
+clearCanvasBtn.addEventListener("click", (e) => {
+	ctx.clearRect(0,0,canvas.width, canvas.height)
+	setCanvasBg()
+})
+
+// SAVE IMG
+
+saveImgBtn.addEventListener("click", () => {
+	const link = document.createElement("a");
+	link.download = `rasul-point${Date.now()}.jpg`;
+	link.href = canvas.toDataURL();
+	link.click()
+})
 
 // STOP DRAW
 const stopDraw = () => {
